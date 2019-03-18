83dd17e0e927e3a6bacd92490ce3512f3ce7ca5f
miradoniaina <miradoeddy@gmail.com>
2019-03-18 08:04:08 +0300

    UI commit

--- src/base.js
+++ src/base.js
@@ -3,9 +3,7 @@ import Rebase from 're-base';
 const base = Rebase.createClass({
     // copier les 3 premières lignes de VOTRE config de votre base !!!
     // NE PRENEZ PAS LES VALEURS QUI SONT ICI !!!
-    apiKey: "AIzaSyCQrvXnOiKs3KUb8WkepYoNVzyPrIH0Pi8",
-    authDomain: "epurestaurants-ede81.firebaseapp.com",
-    databaseURL: "https://epurestaurants-ede81.firebaseio.com",
+    
 
 });
 
